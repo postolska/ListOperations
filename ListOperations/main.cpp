1edb3a39f675ad333284f800f77b90c272e40772
Paulina Ostolska <pa.ostolska@gmail.com>
2022-04-12 19:49:02 +0200

    Initial Commit

---
+++ ListOperations/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ListOperations
+//
+//  Created by Paula Ostolska on 12/04/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
